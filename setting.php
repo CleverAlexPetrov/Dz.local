b6593cf7f886e689353e201153e36e9470b7cfdc
aleksandr <sckrudg@gmail.com>
2020-03-04 00:29:04 +0200

    fix bug

--- setting.php
+++ setting.php
@@ -1,6 +1,6 @@
 <?php
-define('HOST', '');
-define('USER', '');
-define('PASS', '');
-define('DB', '');
+define('HOST', 'localhost');
+define('USER', 'root');
+define('PASS', 'Pass123word%');
+define('DB', 'soul');
 ?>
